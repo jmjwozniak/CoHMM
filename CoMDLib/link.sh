b668e124ca5b6c91fae1c9c60e5efebe85b5140d
Justin M Wozniak <wozniak@mcs.anl.gov>
2015-01-22 13:57:34 -0600

    Fix note

--- CoMDLib/link.sh
+++ CoMDLib/link.sh
@@ -1,13 +1,12 @@
-#!/bin/sh
+#!/bin/sh -e
 
-# Omits eam_memleak.so
+# Link script that omits eam_memleak.so
 
 set -x
 
 rm -fv libCoMD.so
 
-# This can fail:
 cc -shared -fPIC -o libCoMD.so cmdLineParser.o CoMD_lib.o decomposition.o deformation.o eam.o haloExchange.o initAtoms.o linkCells.o ljForce.o mycommand.o parallel.o parseInputs.o performanceTimers.o random.o timestep.o yamlOutput.o
 
-# Check for success:
+# Check/report success:
 ls -l libCoMD.so
