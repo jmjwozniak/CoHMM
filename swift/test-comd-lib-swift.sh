c0ce1e682edfddfa51b2a11e1dc3501fe1d4a781
Justin M Wozniak <wozniak@mcs.anl.gov>
2015-01-22 14:17:05 -0600

    Turn logging/debugging off

--- swift/test-comd-lib-swift.sh
+++ swift/test-comd-lib-swift.sh
@@ -5,4 +5,6 @@ export SWIFT_PATH=$CoHMM_SWIFT_HOME
 
 export CoHMM_HOME=$( dirname $CoHMM_SWIFT_HOME )
 
+export TURBINE_LOG=0 TURBINE_DEBUG=0 ADLB_DEBUG=0
+
 swift-t $CoHMM_SWIFT_HOME/test-comd-lib.swift
