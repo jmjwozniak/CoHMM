da2a220866fac038dbc6efdbb2a609d6f1381fbe
Justin M Wozniak <wozniak@mcs.anl.gov>
2015-01-22 13:22:50 -0600

    Better test

--- swift/test-comd-tcl.sh
+++ swift/test-comd-tcl.sh
@@ -3,4 +3,7 @@
 THIS=$( cd $( dirname $0 ) ; /bin/pwd )
 
 export TCLLIBPATH=$THIS
+
+export CoHMM_HOME=$( dirname $THIS )
+
 tclsh $THIS/test-comd.tcl
--- swift/test-comd.tcl
+++ swift/test-comd.tcl
@@ -12,4 +12,31 @@ if { ! $found } {
     exit 1
 }
 puts "Found CoMD_lib()."
+
+set input [ new_CoMD_input ]
+
+puts "CoHMM_HOME: $env(CoHMM_HOME)"
+
+CoMD_input_potDir_set $input "$env(CoHMM_HOME)/CoMDLib/potentials"
+CoMD_input_potName_set $input "Cu01.eam.alloy"
+CoMD_input_potType_set $input "setfl"
+CoMD_input_doeam_set $input 1
+CoMD_input_nx_set $input 12
+CoMD_input_ny_set $input 12
+CoMD_input_nz_set $input 12
+CoMD_input_nSteps_set $input 50
+CoMD_input_printRate_set $input 1
+CoMD_input_dt_set $input 2.5
+CoMD_input_lat_set $input 3.619
+CoMD_input_energy_set $input -0.296
+CoMD_input_initialDelta_set $input 0.0
+CoMD_input_defGrad_set $input 1.000
+CoMD_input_temperature_set $input 263.450
+
+set result [ CoMD_lib $input ]
+
+set stressXX [ CoMD_return_stressXX_get $result ]
+
+puts "stressXX: $stressXX"
+
 exit 0
